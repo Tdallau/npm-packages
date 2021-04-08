2ceb9c112334a546bd0e3c7f4b972e24e836bcdb
Tdallau <tim@dallau.com>
2021-04-08 09:40:01 +0200

    added some functions to @tdallau/helpers

--- packages/helpers/src/index.ts
+++ packages/helpers/src/index.ts
@@ -1,2 +1,10 @@
+import { Option, Func } from '@tdallau/types';
+
 export const isNumber = (num: number) => !isNaN(num);
 export const isStringNumber = (num: string) => !isNaN(+num);
+
+export const some = <T>(v: T): Option<T> => ({ kind: 'some', value: v});
+export const none = <T>(): Option<T> => ({ kind: 'none'});
+
+export const promisify = <R>(exec: Func<[resolve: Func<[v:R], void>, reject: Func<string, void>],void>): Promise<R> => new Promise(exec);
+
